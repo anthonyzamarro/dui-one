d4efeadada2e7057bd81bcc5ba0f6f6ae7751c54
Anthony Zamarro <azamarro@veracode.com>
2021-10-09 20:16:43 -0400

    add home and signin pages

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,31 @@
+import Form from '../containers/Form';
+import JoinSignIn from '../containers/JoinSignIn';
+import Legal from '../containers/Legal';
+import SignUpWith from '../containers/SignUpWith';
+import Button from '../elements/Button';
+
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <section className="img__container"><img src="https://via.placeholder.com/50" alt="logo" /></section>
+      <main className="container">
+        <JoinSignIn />
+        <SignUpWith 
+          title="SIGN UP WITH..."
+          body="Signing up with social is super quick. No extra passwords to remember - no brain fail. Don't worry, we'd never share any of your data or post anything on your behalf #notevil"
+        />
+        <Form />
+        <Legal />
+        <Button
+          link="www.reddit.com"
+          text="JOIN ASOS"
+          newWindow={true}
+          type="cta"
+        />
+      </main>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/components/pages/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JoinSignIn from "../containers/JoinSignIn";
+
+const SignIn: React.FC = () => {
+    return (
+        <section className="signin__container">
+            <JoinSignIn />
+        </section>
+    )
+}
+
+export default SignIn;
